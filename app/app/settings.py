3dc052fc1aae5ce93c89f98e676a6eac054f7bd6
codigoenlaweb <codeenlaweb@gmail.com>
2022-10-11 23:47:48 -0400

    allowed_host

--- app/app/settings.py
+++ app/app/settings.py
@@ -26,10 +26,11 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'changeme')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(int(os.environ.get('DEBUG', 0)))
 
+ALLOWED_HOSTS = []
 ALLOWED_HOSTS.extend(
     filter(
         None,
-        os.environ.get('DJANGO_ALLOWED_HOSTS', '').split(','),
+        os.environ.get('ALLOWED_HOSTS', '').split(','),
     )
 )
 
